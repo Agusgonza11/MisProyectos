d4eb985506cb8c8b18fb91f7cfb3aa5223ec4c34
Agusgonza11 <82238260+Agusgonza11@users.noreply.github.com>
2023-08-10 16:34:44 -0300

    Add files via upload

---
+++ taller de programacion (rust)/holamundo/target/debug/deps/nombre-9b9fa4d545cab030.d
@@ -0,0 +1,5 @@
+/home/agus/Documentos/taller de programacion/nombre/target/debug/deps/nombre-9b9fa4d545cab030: src/main.rs
+
+/home/agus/Documentos/taller de programacion/nombre/target/debug/deps/nombre-9b9fa4d545cab030.d: src/main.rs
+
+src/main.rs:
---
+++ taller de programacion (rust)/holamundo/target/debug/nombre.d
@@ -0,0 +1,1 @@
+/home/agus/Documentos/taller\ de\ programacion/nombre/target/debug/nombre: /home/agus/Documentos/taller\ de\ programacion/nombre/src/main.rs
